6e97c14be85d205d068f349fb9698f812b54ba4c
Marcel Durgante <info@marceldurgante.dev>
2022-10-11 22:59:17 +0200

    Symbol explanation with examples

---
+++ primitive_types/symbol.js
@@ -0,0 +1,36 @@
+let id = Symbol("id");
+let id2 = Symbol("id");
+
+const NAME = Symbol();
+const SURNAME = Symbol();
+const GREET = Symbol();
+
+const person = {
+  [NAME]: "Marcel1",
+  [SURNAME]: "Durgante1",
+  NAME: "Marcel2",
+  age: 35
+};
+
+/* console.log(person);
+ */
+person.SURNAME = "Durgante2";
+
+/* console.log(person);
+console.log(person.NAME);
+console.log(person[NAME]); */
+
+person[GREET] = function () {
+  console.log(`Hello`);
+};
+/* console.log('after greet', person);
+ */
+person[GREET]();
+
+for (let property in person) {
+  console.log(property);
+console.log(person[property]);
+}
+
+
+console.log(Object.getOwnPropertySymbols('getOwnProp', person)); 
